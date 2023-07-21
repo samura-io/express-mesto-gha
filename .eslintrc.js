3dc491cd52a6f828accc53c50238ac1b5f8ce051
Nikita Aleksankin <NTL5TIA@gmail.com>
2023-07-22 02:47:46 +0300

    added auth

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,5 @@ module.exports = {
   },
   rules: {
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
-    'consistent-return': 'off', // Это Уркин, насильник местный. Нехороший!
   },
 };
